50bf633c0eb4a6ec61fec358cc38a9f393035c5c
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-11-12 09:30:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const listItems = document.querySelectorAll(".expertise-list li");
+
+listItems.forEach((item) => {
+  const content = item.querySelector(".expertise-list-item-desc");
+  const icon = item.querySelector("img");
+
+  item.addEventListener("click", () => {
+    content.classList.toggle("show-list-item-desc");
+    item.classList.toggle("show-expertise-list-item");
+    icon.classList.toggle("expertise-list-icon-spin");
+  });
+});
